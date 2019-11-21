20d30edcbb14c415cb16a134d313a21d72a32d84
claudyne <claudyneuy98@gmail.com>
2019-11-21 15:09:31 +0800

    added file encryption in key detaching process

--- key_detached.py
+++ key_detached.py
@@ -1,8 +1,9 @@
 import subprocess
 
 #generation of new password and hash and storing new pw
-cmd1 = subprocess.Popen(["python", "upload_sketch.py"])
+#cmd1 = subprocess.Popen(["python", "upload_sketch.py"])
 
 
 #call encryption here
 print("Encrypting here.")
+cmd2 = subprocess.Popen(["python", "file-encryption.py"])
