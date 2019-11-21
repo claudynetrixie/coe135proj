21e6d6c2e6c0a9ace670b8777ccbf88e70564001
claudyne <claudyneuy98@gmail.com>
2019-11-21 10:25:27 +0800

    added encryption for multiple files

--- file-encryption.py
+++ file-encryption.py
@@ -2,23 +2,54 @@ import cryptography
 
 from cryptography.fernet import Fernet
 
+from os import listdir
+from os.path import isfile, join
+
+import os
+import itertools
+
+mypath = "/home/claudyne/Desktop/encrypted"
+
+#Getting Input Files
+input_files = [f for f in listdir(mypath) if isfile (join(mypath,f))]
+num_file = len(input_files)
+
+output_files = []
+input_paths = []
+output_paths = []
+
+for i in input_files:
+	output_files.append((str(os.path.splitext(i)[0])) + '.enc')
+	input_paths.append(mypath +"/" +i)
+
+for i in output_files:
+	output_paths.append(mypath + "/" + i)
+
+#Key Generation
 key = Fernet.generate_key()
 print(key)
-
+#Key Storage
 file = open('key.key', 'wb')
 file.write(key)
 file.close()
 
-input_file = 'test.txt'
-output_file = 'text.enc'
+data = []
+encrypted = []
 
-with open(input_file, 'rb') as f:
-    data = f.read()
+for i in input_paths:
+	with open(i, 'rb') as f:
+	    data.append(f.read())
 
 fernet = Fernet(key)
-encrypted = fernet.encrypt(data)
 
-with open(output_file, 'wb') as f:
-    f.write(encrypted)
+for i in data:
+	encrypted.append(fernet.encrypt(i))
 
+
+for (i,x) in zip(output_paths, encrypted):
+	with open(i, 'wb') as f:
+	    f.write(x)
+
+for i in input_paths:
+	os.remove(i)
 
