37eef73c8467b8d16c19aac6ab8bff8ede3e0071
claudyne <claudyneuy98@gmail.com>
2019-11-21 09:26:52 +0800

    creating a log file for encrypted key from arduino

--- serial1.py
+++ serial1.py
@@ -1,16 +1,21 @@
 import serial
 import time
+from os.path import expanduser
+import datetime
+import os
 
 arduino = serial.Serial("/dev/ttyUSB0", 9600, timeout = 5, bytesize = serial.EIGHTBITS, parity = serial.PARITY_NONE, stopbits = serial.STOPBITS_ONE, xonxoff = 0, rtscts = 0);
 
 
 time.sleep(2)
 data = arduino.readline()
-time.sleep(1)
+time.sleep(2)
 
 print (data)
 
-
+file = open(expanduser("~") + '/Desktop/script.log', 'a')
+file.write("Encrypted Key: " + data + " " + str(datetime.datetime.now()) + " " + str(os.getpid()) + "\n")
+file.close()
 
 
 
