a7983c095b5200b4f418eaa980c68ecb57828ec6
claudyne <claudyneuy98@gmail.com>
2019-11-28 23:56:20 +0800

    updated script file

--- authduino_installer.sh
+++ authduino_installer.sh
@@ -1,5 +1,12 @@
 #! /bin/sh
 
 unzip Authduino.zip
-
+remove Authduino.zip
 cp -i -R  Authduino /home/$USER
+cd /home/$USER/Authduino
+mkdir /home/$USER/AuthduinoDocs
+
+sudo cp -i /home/$USER/Authduino/authduino.py /usr/bin
+
+chmod +r /usr/bin/authduino.py
+
