3e579f8182300dcae25d71d986a1f7065feed379
psbalucan <psbalucan@eee.upd.edu.ph>
2019-11-29 10:55:12 +0800

    Added service creation to installer file

--- serve..sh
+++
@@ -1,3 +0,0 @@
-#! /bin/sh
-echo "[Unit]\nDescription=Dummy Service\nAfter=multi-user.target\nConflicts=getty@tty1.service\n\n[Service]\nType=simple\nUser=$USER\nExecStart=/usr/bin/python -u /usr/bin/authduino.py\n\n[Install]\nWantedBy=multi-user.target"  > test.service
-echo $USER
--- serve.sh
+++ serve.sh
@@ -1,13 +1,4 @@
 #! /bin/sh
-unzip Authduino.zip
-remove Authduino.zip
-cp -i -R  Authduino /home/$USER
-cd /home/$USER/Authduino
-mkdir /home/$USER/AuthduinoDocs
-
-sudo cp -i /home/$USER/Authduino/authduino.py /usr/bin
-
-sudo chmod +r /usr/bin/authduino.py
 
 echo $USER
 
@@ -27,8 +18,3 @@ echo "[Install]" >> authduino.service
 echo "WantedBy=multi-user.target" >> authduino.service
 sudo cp -i authduino.service /lib/systemd/system
 rm authduino.service
-
-sudo systemctl daemon-reload
-sudo systemctl enable authduino.service
-sudo systemctl start authduino.service 
-sudo service authduino status
